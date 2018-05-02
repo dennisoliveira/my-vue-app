b3dc0c76e8b3c3fcea50fbe8057437605aa0d6ea
Dennis de Oliveira <contato@dennisoliveira.com.br>
2018-05-01 21:46:24 -0300

    Adicionado novo componente para testar outra rota

---
+++ src/components/TestRouter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Test Router (Testando)
+    </div>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+
+
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 import HelloWorldRouter from './components/HelloWorldRouter.vue'
+import TestRouter from './components/TestRouter.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes: [{path: '/', component: HelloWorldRouter}]
+  routes: [
+    {path: '/', component: HelloWorldRouter},
+    {path: '/test', component: TestRouter}
+  ]
 })
 
 new Vue({
